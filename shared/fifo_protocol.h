03749e793e934c23b6eff99d3f8dab70912a76ee
Tomunek <kowalczyk.tomek11@gmail.com>
2026-07-27 20:29:38 +0200

    Style

--- shared/fifo_protocol.h
+++ shared/fifo_protocol.h
@@ -6,7 +6,7 @@
 // Core 1 → Core 0
 // Word 1: (FIFO_MSG_VERIFY << 24) | (key_id & 0xFFFF)
 // Word 2: totp_code
-#define FIFO_MSG_VERIFY   0x01U
+#define FIFO_MSG_VERIFY 0x01U
 
 // Core 0 → Core 1
 #define FIFO_RESULT_GRANTED 0x01U
